fee2e50e081992278a8fabffebbdcb4cb3f10ccc
Oscar Key <oscar.t.key@gmail.com>
2019-06-14 22:16:14 +0100

    Specify gpytorch version as latest breaks us.

--- setup.py
+++ setup.py
@@ -24,7 +24,8 @@ setup(
                     'visualization': ['matplotlib',
                              'pygame'],
                     'ssm_gpy':  ['GPy'],
-                    'ssm_pytorch': ['gpytorch',
+                    # TODO: upgrade to latest gpytorch (meant to be faster on small test sets!)
+                    'ssm_pytorch': ['gpytorch==0.3.2',
                                     'torch',
                                     'hessian @ https://github.com/mariogeiger/hessian/tarball/master#egg=hessian']},
 
