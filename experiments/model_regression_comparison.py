859509f324fe92621a3c7acc55c947bfda5977ea
Oscar Key <oscar.t.key@gmail.com>
2019-07-03 18:15:40 +0100

    Fix broken experiment configs.

--- experiments/model_regression_comparison.py
+++ experiments/model_regression_comparison.py
@@ -163,8 +163,8 @@ def regression_comparison_main(_run):
     run = functools.partial(_run_mcdropout, x_train=x_train, y_train=y_train, x_test=x_test)
 
     # Search over training iterations and repeats for gal implementation.
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=1, dropout_type='concrete', dropout_p=0.1))
-    run(_conf(i=1, impl='gal', hidden_layer_size=20, training_iter=1, dropout_type='concrete', dropout_p=0.1))
+    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=3000, dropout_type='concrete', dropout_p=0.1))
+    run(_conf(i=1, impl='gal', hidden_layer_size=20, training_iter=3000, dropout_type='concrete', dropout_p=0.1))
     run(_conf(i=2, impl='gal', hidden_layer_size=20, training_iter=3000, dropout_type='concrete', dropout_p=0.1))
     run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=5000, dropout_type='concrete', dropout_p=0.1))
     run(_conf(i=1, impl='gal', hidden_layer_size=20, training_iter=5000, dropout_type='concrete', dropout_p=0.1))
