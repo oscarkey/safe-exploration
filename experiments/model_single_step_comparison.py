2587c99e0217d6126b9aa3204e63b04b9bc0e3c5
Oscar Key <oscar.t.key@gmail.com>
2019-07-05 15:29:23 +0100

    Working version of single step uncertainty experiments.

--- experiments/model_single_step_comparison.py
+++ experiments/model_single_step_comparison.py
@@ -1,5 +1,7 @@
 """Demonstrates the uncertainty estimates of gp vs mc dropout on single steps in the environment."""
-from typing import Tuple
+import functools
+import os
+from typing import Tuple, Dict
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,10 +12,19 @@ from numpy import ndarray
 from experiments import sacred_helper
 from safe_exploration.environments import InvertedPendulum, Environment
 from safe_exploration.gp_reachability_pytorch import onestep_reachability
-from safe_exploration.ssm_cem import GpCemSSM, CemSSM, McDropoutSSM
-from visualization import utils_visualization
+from safe_exploration.ssm_cem.gal_concrete_dropout import GalConcreteDropoutSSM
+from safe_exploration.ssm_cem.ssm_cem import GpCemSSM, CemSSM
+from safe_exploration.visualization import utils_visualization
 
 ex = sacred_helper.get_experiment()
+
+
+def _sample(env: Environment, action: ndarray) -> ndarray:
+    # Set std to zero so we end up exactly at the origin
+    initial = env.reset(mean=np.array([0., 0.]), std=np.array([0., 0.]))
+    assert np.allclose(initial, np.array([0., 0.]))
+    _, new_state, _, _ = env.step(action)
+    return new_state
 
 
 def _gather_samples(env: Environment) -> Tuple[ndarray, ndarray]:
@@ -25,47 +36,106 @@ def _gather_samples(env: Environment) -> Tuple[ndarray, ndarray]:
 
     y_train = np.empty((num_samples, env.n_s))
     for i in range(x_train.shape[0]):
-        # Set std to zero so we end up exactly at the origin
-        initial = env.reset(mean=np.array([0., 0.]), std=np.array([0., 0.]))
-        assert np.allclose(initial, np.array([0., 0.]))
-        _, new_state, _, _ = env.step(actions[i])
-        y_train[i] = new_state
+        y_train[i] = _sample(env, actions[i])
 
     return x_train, y_train
 
 
-def _run_experiment(env: Environment, ssm: CemSSM, x_train, y_train, x_test):
+def _get_color(i) -> str:
+    return f'C{i}'
+
+
+def _pretty_print_metrics(metrics: Dict[str, float]) -> str:
+    result = ''
+    for k, v in metrics.items():
+        result += f'{k}: {v:.4f}; '
+    return result
+
+
+def _train_and_plot_for_ssm(env: Environment, ssm: CemSSM, x_train, y_train, x_test, axes, linestyle='-'):
     ssm.update_model(torch.tensor(x_train), torch.tensor(y_train), opt_hyp=True)
 
     states = torch.zeros((x_test.shape[0], env.n_s))
     actions = torch.tensor(x_test)
 
-    ps, qs, _ = onestep_reachability(states, ssm, actions, torch.tensor(env.l_mu), torch.tensor(env.l_sigm))
-
-    axes = plt.axes()
-    axes.scatter([0], [0], color='r', s=4)
-    axes.scatter(y_train[:, 0], y_train[:, 1], s=2)
+    ps, qs, _ = onestep_reachability(states, ssm, actions, torch.tensor(env.l_mu), torch.tensor(env.l_sigm), verbose=0)
 
     for i in range(ps.size(0)):
         p = ps[i].unsqueeze(1).detach().cpu().numpy()
         q = qs[i].detach().cpu().numpy()
-        utils_visualization.plot_ellipsoid_2D(p, q, axes, n_points=30)
+        utils_visualization.plot_ellipsoid_2D(p, q, axes, n_points=30, color=_get_color(i), linewidth=0.5,
+                                              linestyle=linestyle)
 
-    plt.show()
+
+def _run_experiment(env: Environment, x_train, y_train, x_test, save_to_file, conf):
+    # Adjust the axes rectange to make room for the footnote text containing dropout probabilities.
+    ax1 = plt.axes([0.125, 0.15, 0.885, 0.85])
+
+    ax1.set_xlabel('angular velocity')
+    ax1.set_ylabel('angle from vertical')
+
+    ax1.scatter([0], [0], color='black', s=3)
+    ax1.scatter(y_train[:, 0], y_train[:, 1], color='grey', s=2)
+
+    for i in range(x_test.shape[0]):
+        y_test = _sample(env, x_test[i])
+        ax1.scatter(y_test[0], y_test[1], color=_get_color(i), s=4)
+
+    mc_dropout = GalConcreteDropoutSSM(conf, env.n_s, env.n_u)
+    _train_and_plot_for_ssm(env, mc_dropout, x_train, y_train, x_test, axes=ax1)
+
+    gp = GpCemSSM(conf, env.n_s, env.n_u)
+    _train_and_plot_for_ssm(env, gp, x_train, y_train, x_test, axes=ax1, linestyle='--')
+
+    plt.figtext(0, 0, _pretty_print_metrics(mc_dropout.collect_metrics()), wrap=True)
+
+    if save_to_file:
+        folder = 'results_single_step'
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        file_name = f'{folder}/{conf.name}.png'
+        plt.savefig(file_name)
+        plt.gcf()
+    else:
+        plt.show()
+
+
+@ex.capture
+def _conf(_run, i, training_iter, lengthscale):
+    conf = EasyDict(_run.config)
+    conf.mc_dropout_training_iterations = training_iter
+    conf.mc_dropout_lengthscale = lengthscale
+
+    conf.name = f'ls={lengthscale:.6f}_iter={training_iter}_{i}'
+
+    return conf
 
 
 @ex.automain
 def single_step_compare_main(_run):
     torch.set_default_dtype(torch.double)
 
-    conf = EasyDict(_run.config)
-    env = InvertedPendulum()
+    env = InvertedPendulum(verbosity=0)
 
     x_train, y_train = _gather_samples(env)
-    x_test = np.expand_dims(np.arange(env.u_min, env.u_max, 0.1), 1)
+    x_test = np.expand_dims(np.arange(env.u_min, env.u_max, 0.2), 1)
 
-    mc_dropout = McDropoutSSM(conf, env.n_s, env.n_u)
-    _run_experiment(env, mc_dropout, x_train, y_train, x_test)
+    save_to_file = True
+    run = functools.partial(_run_experiment, env, x_train, y_train, x_test, save_to_file)
 
-    mc_dropout = GpCemSSM(conf, env.n_s, env.n_u)
-    _run_experiment(env, mc_dropout, x_train, y_train, x_test)
+    run(_conf(i=0, training_iter=3000, lengthscale=1e-4))
+    run(_conf(i=1, training_iter=3000, lengthscale=1e-4))
+    run(_conf(i=2, training_iter=3000, lengthscale=1e-4))
+    run(_conf(i=0, training_iter=6000, lengthscale=1e-4))
+    run(_conf(i=1, training_iter=6000, lengthscale=1e-4))
+    run(_conf(i=2, training_iter=6000, lengthscale=1e-4))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-4))
+    run(_conf(i=1, training_iter=9000, lengthscale=1e-4))
+    run(_conf(i=2, training_iter=9000, lengthscale=1e-4))
+
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-1))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-2))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-3))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-4))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-5))
+    run(_conf(i=0, training_iter=9000, lengthscale=1e-6))
