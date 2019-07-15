23b995aa71d1baef3de8992921343166719e7a68
Oscar Key <oscar.t.key@gmail.com>
2019-07-15 21:32:56 +0100

    Update regression experiments to use SSM gp (and so nn kernel).

--- experiments/model_regression_comparison.py
+++ experiments/model_regression_comparison.py
@@ -1,56 +1,21 @@
 """Script to compare exact gp and mc dropout models on a simple regression task."""
 import functools
 import os
-from typing import Optional
 
 import gpytorch
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from easydict import EasyDict
-from torch import Tensor
 
 from experiments import sacred_helper
 from safe_exploration.ssm_cem.gal_concrete_dropout import GalConcreteDropoutSSM
-from safe_exploration.ssm_cem.ssm_cem import McDropoutSSM
+from safe_exploration.ssm_cem.gp_ssm_cem import GpCemSSM
+from safe_exploration.ssm_cem.ssm_cem import McDropoutSSM, CemSSM
 from safe_exploration.ssm_pytorch.gaussian_process import ZeroMeanWithGrad
 from safe_exploration.utils import get_device
 
 ex = sacred_helper.get_experiment()
-
-
-class GP:
-    def __init__(self, x_train, y_train):
-        self._likelihood = gpytorch.likelihoods.GaussianLikelihood()
-        self._model = ExactGPModel(x_train, y_train, self._likelihood)
-
-    def predict(self, x: Tensor):
-        self._likelihood.eval()
-        self._model.eval()
-        pred = self._model(x)
-        return pred.mean.unsqueeze(1), pred.variance.unsqueeze(1)
-
-    def train(self, xs: Tensor, ys: Tensor, iterations: int) -> None:
-        self._likelihood.train()
-        self._model.train()
-
-        # self._model.parameters() includes GaussianLikelihood parameters
-        optimizer = torch.optim.Adam([{'params': self._model.parameters()}, ], lr=0.1)
-
-        # "Loss" for GPs - the marginal log likelihood
-        mll = gpytorch.mlls.ExactMarginalLogLikelihood(self._likelihood, self._model)
-
-        for i in range(iterations):
-            # Zero gradients from previous iteration
-            optimizer.zero_grad()
-            # Output from model
-            output = self._model(xs)
-            # Calc loss and backprop gradients
-            loss = -mll(output, ys).sum()
-            loss.backward()
-            optimizer.step()
-            if i % 500 == 0:
-                print(f'{i}: {loss.item()}')
 
 
 class ExactGPModel(gpytorch.models.ExactGP):
@@ -65,28 +30,26 @@ class ExactGPModel(gpytorch.models.ExactGP):
         return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
-def _plot(x_train, y_train, x_test, preds, text: str, file_name: Optional[str] = None):
+def _plot(axes, x_train, y_train, x_test, preds, text: str):
     x_train = x_train.detach().cpu().numpy()
     y_train = y_train.detach().cpu().numpy()
     x_test = x_test.detach().cpu().numpy()
-
-    axes = plt.axes()
 
     plt.axis([-12, 12, -4, 4])
 
     # Plot the base sin function.
     xs = np.arange(-12, 12, 0.05)
-    axes.plot(xs, np.sin(xs))
+    axes.plot(xs, _true_func(xs), color='C0')
 
     # Plot the trainin data.
-    axes.scatter(x_train, y_train, zorder=10, s=4)
+    axes.scatter(x_train, y_train, zorder=10, s=4, color='C0')
 
     pred_mean, pred_var = preds
-    pred_mean = pred_mean.squeeze(1).detach().cpu().numpy()
-    pred_std = pred_var.squeeze(1).sqrt().detach().cpu().numpy()
+    pred_mean = pred_mean.squeeze().detach().cpu().numpy()
+    pred_std = pred_var.squeeze().sqrt().detach().cpu().numpy()
 
     # Plot the mean line.
-    axes.plot(x_test, pred_mean)
+    axes.plot(x_test, pred_mean, color='C1')
 
     axes.text(0, 0, text, transform=axes.transAxes, wrap=True)
 
@@ -95,44 +58,45 @@ def _plot(x_train, y_train, x_test, preds, text: str, file_name: Optional[str] =
         axes.fill_between(x_test, (pred_mean - i * pred_std).flat, (pred_mean + i * pred_std).flat, color="#dddddd",
                           alpha=1.0 / i)
 
-    if file_name is not None:
-        print(f'Saved fig to {file_name}')
+
+def _get_ssm(conf) -> CemSSM:
+    if conf.cem_ssm == 'exact_gp':
+        return GpCemSSM(conf, state_dimen=1, action_dimen=0)
+    elif conf.cem_ssm == 'mc_dropout':
+        return McDropoutSSM(conf, state_dimen=1, action_dimen=0)
+    elif conf.cem_ssm == 'mc_dropout_gal':
+        return GalConcreteDropoutSSM(conf, state_dimen=1, action_dimen=0)
+    else:
+        raise ValueError(f'Unknown ssm {conf.ssm_type}')
+
+
+def _run_experiment(conf, x_train, y_train, x_test, save_to_file):
+    ssm = _get_ssm(conf)
+
+    ssm.update_model(x_train.unsqueeze(1), y_train.unsqueeze(1), opt_hyp=True)
+
+    preds = ssm.predict_raw(x_test.unsqueeze(1))
+
+    axes = plt.axes()
+    # plt.tight_layout()
+    # plt.plot(xs, ys, color='C7')
+    _plot(axes, x_train, y_train, x_test, preds, text="")
+
+    if save_to_file:
+        folder = 'results_regression'
+        if not os.path.isdir(folder):
+            os.mkdir(folder)
+        file_name = f'{folder}/{conf.name}.png'
         plt.savefig(file_name)
         plt.clf()
+        print(f'Saved fig to {file_name}')
     else:
         plt.show()
 
 
-def _run_mcdropout(conf, x_train, y_train, x_test):
-    if conf.impl == 'lib':
-        mcdropout = McDropoutSSM(conf, state_dimen=1, action_dimen=0)
-    elif conf.impl == 'gal':
-        mcdropout = GalConcreteDropoutSSM(conf, state_dimen=1, action_dimen=0)
-    else:
-        raise ValueError(f'Unknown impl {conf.impl}')
-
-    mcdropout._train_model(x_train.unsqueeze(1), y_train)
-
-    folder = 'results_regression'
-    if not os.path.isdir(folder):
-        os.mkdir(folder)
-    file_name = f'{folder}/{conf.name}.png'
-    # file_name = None
-    text = f'{conf.name}: {mcdropout.collect_metrics()}'
-    _plot(x_train, y_train, x_test, mcdropout.predict_raw(x_test.unsqueeze(1)), text, file_name)
-    print(f'final p \'{conf.name}\': {mcdropout.collect_metrics()}')
-
-
-def _run_gp(x_train, y_train, x_test):
-    gp = GP(x_train, y_train)
-    gp.train(x_train, y_train, iterations=50)
-
-    _plot(x_train, y_train, x_test, gp.predict(x_test.unsqueeze(1)))
-
-
 @ex.capture
-def _conf(_run, i: int, impl: str, hidden_layer_size: int, training_iter: int, dropout_type: str = 'concrete',
-          dropout_p: float = 0.1, input_dropout=True, length_scale=1e-4):
+def _dropout_conf(_run, i: int, impl: str, hidden_layer_size: int, training_iter: int, dropout_type: str = 'concrete',
+                  dropout_p: float = 0.1, input_dropout=True, length_scale=1e-4):
     conf = EasyDict(_run.config)
     conf.mc_dropout_predict_std = True
     conf.mc_dropout_reinitialize = True
@@ -144,45 +108,47 @@ def _conf(_run, i: int, impl: str, hidden_layer_size: int, training_iter: int, d
     conf.mc_dropout_on_input = input_dropout
     conf.mc_dropout_lengthscale = length_scale
 
-    conf.impl = impl
-
     conf.name = f'{impl}_{dropout_type}_ls={length_scale:.5f}' \
         f'_iter={training_iter}_indrop={input_dropout}_hidden={hidden_layer_size}_p={dropout_p:.3f}_{i}'
 
     return conf
 
 
+@ex.capture
+def _gp_conf(_run):
+    conf = EasyDict(_run.config)
+    conf.cem_ssm = 'exact_gp'
+    conf.exact_gp_kernel = 'rbf'
+    conf.exact_gp_training_iterations = 1000
+
+    conf.name = f'no_name'
+
+    return conf
+
+
+def _true_func(x):
+    return np.sin(x - 0.8) + 0.5  # return 0.2 * x + .5
+
+
 @ex.automain
 def regression_comparison_main(_run):
+    torch.set_default_dtype(torch.double)
+
     conf = EasyDict(_run.config)
 
-    # Create x training data between -4 and -1 and 1 and 4, so we have a gap between the two sections.
-    x_train = torch.cat([torch.rand(10) * 3 - 4, torch.rand(10) * 3 + 1])
-    y_train = torch.sin(x_train)
+    x_train = np.concatenate([np.arange(-2, -0, 0.2), np.arange(3, 4, 0.2)])
+    y_train = _true_func(x_train)
+    x_train = torch.tensor(x_train)
+    y_train = torch.tensor(y_train)
+
     x_test = torch.linspace(-12, 12, 160)
 
     x_train = x_train.to(get_device(conf))
     y_train = y_train.to(get_device(conf))
     x_test = x_test.to(get_device(conf))
 
-    run = functools.partial(_run_mcdropout, x_train=x_train, y_train=y_train, x_test=x_test)
+    run = functools.partial(_run_experiment, x_train=x_train, y_train=y_train, x_test=x_test, save_to_file=False)
 
-    # Search over hidden layer sizes
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=1, impl='gal', hidden_layer_size=20, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=2, impl='gal', hidden_layer_size=20, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=0, impl='gal', hidden_layer_size=64, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=1, impl='gal', hidden_layer_size=64, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=2, impl='gal', hidden_layer_size=64, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=0, impl='gal', hidden_layer_size=128, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=1, impl='gal', hidden_layer_size=128, training_iter=9000, dropout_type='concrete'))
-    run(_conf(i=2, impl='gal', hidden_layer_size=128, training_iter=9000, dropout_type='concrete'))
+    run(_gp_conf())
 
-    # Search over length scale.
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e1))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e0))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e-1))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e-2))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e-3))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e-4))
-    run(_conf(i=0, impl='gal', hidden_layer_size=20, training_iter=9000, length_scale=1e-5))
+    return
